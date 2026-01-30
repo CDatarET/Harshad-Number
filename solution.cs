b445d4a8dba911003e2c4d43f21d9009935d29ee
Chaitanya Datar <datar.chaitanya@gmail.com>
2026-01-30 23:54:02 +0530

    Create solution.cs

---
+++ solution.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int SumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0;
+        int n = x;
+        while(n > 0){
+            sum += n % 10;
+            n /= 10;
+        }
+
+        if(x % sum == 0){
+            return(sum);
+        }
+
+        return(-1);
+    }
+}
